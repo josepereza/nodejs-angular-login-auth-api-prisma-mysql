87a8b99e626ce950588e2599da0cf3bee92d8725
josepereza <josepereza66@gmail.com>
2022-03-09 18:23:48 +0100

    crear guard

--- angular-login/src/app/app-routing.module.ts
+++ angular-login/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RutasProtegidasGuard } from './guards/rutas-protegidas.guard';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
@@ -7,7 +8,7 @@ import { SignupComponent } from './pages/signup/signup.component';
 const routes: Routes = [
   {path:'', pathMatch:'full', redirectTo:'login'},
   {path:'login', component:LoginComponent},
-  {path:'listado', component:ListadoComponent},
+  {path:'listado', component:ListadoComponent, canActivate:[RutasProtegidasGuard]},
   {path:'signup', component:SignupComponent}
 ];
 
